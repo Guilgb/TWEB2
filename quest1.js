d1ecae38e3c06c3454d3253cdd2dea714bd5e4fb
guilherme <guilherme.gb@hotmail.com>
2022-10-07 10:04:03 -0300

    Quest 1 and 2

---
+++ quest1.js
@@ -0,0 +1,6 @@
+const num_int = [1, 2, 3, 4, 5, 6];
+
+num_int.forEach(element => {
+    
+    console.log(element**2)
+});
---
+++ quest2.js
@@ -0,0 +1,8 @@
+const num = 6;
+if(num > 0){
+    let soma = 0;
+    for(i=0; i<= num; i++){
+        soma = soma + i
+    }
+    console.log(soma)
+}

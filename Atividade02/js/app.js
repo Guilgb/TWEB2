73072ae5c8da4e4be72ebe3a8e351b74c467f80f
Guilherme <guilherme.gb@hotmail.com>
2022-11-17 09:59:06 -0300

    Não funciona

--- Atividade02/js/app.js
+++ Atividade02/js/app.js
@@ -1,13 +1,19 @@
-
 let tarefa = document.getElementById("tarefa")
 let button = document.getElementById("add_tarefa");
+let father = document.getElementsByClassName("sub-list")
 
-button.onclick = function(e){
+button.onclick = function(){ {evento}};
+function evento(){
     e.preventDefault();
 
-    const li = document.createElement("li");
+    let child = father.firstChild;
+    
+    let li = document.createElement("li");
     li.setAttribute("class", "sub-list");
     li.innerHTML = `${tarefa.value}`;
 
+    child.insertBefore(li, child);
+    tarefa.value = "";
+
 }
-button.addEventListener("click", e);
+button.addEventListener("click", evento);

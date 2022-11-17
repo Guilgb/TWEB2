09375da92ab215b5c3816108dd732943397e9ecf
Guilherme <guilherme.gb@hotmail.com>
2022-11-17 11:27:41 -0300

    Tarefa na fila

--- Atividade02/js/app.js
+++ Atividade02/js/app.js
@@ -1,7 +1,23 @@
-const button = document.querySelector("#add_tarefa")
-const input = document.querySelector("#tarefa")
-const father = document.querySelector(".sub-list")
+const button = document.querySelector("#add_tarefa");
+const load = document.querySelector("#a-fazer");
+const finish = document.querySelector("#feito");
+document.querySelector("#tarefa").focus
 
 const addTask = function(event){
     if(event) event.preventDefault();
+
+    const task = document.querySelector("#tarefa").value
+
+    if(task > 1 ){
+        return
+    }
+
+    const newTask = document.createElement("li")
+    newTask.innerText = task
+
+    load.appendChild(newTask)
+    
+    document.querySelector("#tarefa").value = ""
 }
+
+button.addEventListener("click", addTask)

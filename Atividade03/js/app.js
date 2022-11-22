6d0900f846675ff605f5137b487ea532c00677b7
Guilherme <guilherme.gb@hotmail.com>
2022-11-22 20:58:55 -0300

    Insert post

--- Atividade03/js/app.js
+++ Atividade03/js/app.js
@@ -1,1 +1,41 @@
-// Adicione seu código aqui
+button = document.querySelector(".tweetBox__tweetButton")
+input = document.querySelector("#input-text")
+post = document.querySelector('#post')
+
+button.onclick = function(event){
+    event.preventDefault();
+
+    const newPost =   `<div class="post">
+        <div class="post__avatar">
+        <img
+            src="https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png"
+            alt=""
+        />
+        </div>
+        <div class="post__body">
+        <div class="post__header">
+            <div class="post__headerText">
+            <h3>
+                Somanath Goudar
+                <span class="post__headerSpecial"
+                ><span class="material-icons post__badge"> verified </span>@somanathg</span
+                >
+                <button id="btn-delete" type="submit" class="editar">Deletar</button>
+                <button id="btn-edit" type="submit" class="editar">Editar</button>
+            </h3>
+            </div>
+            <div class="post__headerDescription">
+            <p>${input.value}</p>
+            </div>
+        </div>
+        <div class="post__footer">
+            <span class="material-icons"> repeat </span>
+            <span class="material-icons"> favorite_border </span>
+            <span class="material-icons"> publish </span>
+        </div>
+        </div>
+    </div>'`
+
+    post.insertAdjacentHTML("afterbegin", newPost)
+    input.value = ""
+}

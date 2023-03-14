d8e7cb6aa321a842cae85773f746457eef0432f8
Guilherme Bezerra <88347940+Guilgb@users.noreply.github.com>
2023-03-14 14:56:47 -0300

    pronjeto pronto

--- magic (1)/magic/src/pages/Cards/index.jsx
+++ magic (1)/magic/src/pages/Cards/index.jsx
@@ -6,15 +6,35 @@ const baseURL = import.meta.env.VITE_URL;
 
 const Cards = () => {
   const [cards, setCards] = useState([]);
+  const [pageController, setPageController] = useState(1);
 
+  const renderPage = (type) => {
+      if (pageController === 0 ) return
+      if (type === 'next'){
+          setPageController(pageController + 1)
+        }else{
+            setPageController(pageController - 1)
+        }
+        
+    console.log('pageController:', Number(pageController))
+
+  }  
   const getAllCards = async (url) => {
     const cards = await scryfail.get(url).then((res) => res);
 
     setCards(cards.data.data);
   };
 
+
+
+    useEffect(() => {
+        const cardsURL = `${baseURL}/cards/search?format=json&include_extras=false&include_multilingual=false&include_variations=false&order=name&limit=20&page=${pageController}&q=color<%3Dwubrg+%28game%3Apaper%29&unique=cards`;
+        getAllCards(cardsURL);
+        console.log(pageController)
+      }, [pageController]);
+
   useEffect(() => {
-    const cardsURL = `${baseURL}/cards/search?as=grid&order=name&q=color<%3DWUBRG+%28game%3Apaper%29`;
+    const cardsURL = `${baseURL}/cards/search?format=json&include_extras=false&include_multilingual=false&include_variations=false&order=name&page=2&q=color<%3Dwubrg+%28game%3Apaper%29&unique=cards`;
     getAllCards(cardsURL);
   }, []);
 
@@ -26,6 +46,8 @@ const Cards = () => {
                 {cards && cards.map((card) => <MagicCard card={card} />)}
             </div>
         </div>
+        <button className="butao" disabled={ pageController === 1 ? true : false} onClick={() => {renderPage('back' )}}>Prev</button>
+        <button className="butao" onClick={() => {renderPage('next')}}>Next</button>
     </div>
   );
 };
